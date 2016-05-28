b86218c783b1e476736ab4d71189417427b28be0
Sequarius <sequarius@gmail.com>
2016-05-28 23:05:03 +0800

    Simple wiewholder

---
+++ library/src/main/java/com/seuqarius/squarewheels/widget/recyclerview/SimpleDataSetViewHolder.java
@@ -0,0 +1,13 @@
+package com.seuqarius.squarewheels.widget.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Sequarius on 2016/5/28.
+ */
+public class SimpleDataSetViewHolder extends RecyclerView.ViewHolder{
+    public SimpleDataSetViewHolder(View itemView) {
+        super(itemView);
+    }
+}
---
+++ library/src/main/java/com/seuqarius/squarewheels/widget/recyclerview/SingleDataSetRecyclerAdapter.java
@@ -0,0 +1,10 @@
+package com.seuqarius.squarewheels.widget.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Sequarius on 2016/5/28.
+ */
+public abstract class SingleDataSetRecyclerAdapter<T> extends RecyclerView.Adapter<SimpleDataSetViewHolder> {
+
+}
